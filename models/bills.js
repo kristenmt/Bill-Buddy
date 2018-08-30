4378dd7767b32c0dce4d8a0323b6664bba44a7da
Jennifer Cobb <jenn.cobb.m@gmail.com>
2018-08-30 14:42:05 -0400

    handlebars, displaying dummy data to the page

--- models/bills.js
+++ models/bills.js
@@ -1,13 +1,12 @@
 module.exports = function(sequelize, DataTypes) {
-    var Bills = sequelize.define("Bills", {
-      userID: DataTypes.INTEGER,
-      name: DataTypes.STRING,
-      amount: DataTypes.INTEGER,
-      dueDay: DataTypes.INTEGER,
-      URL: DataTypes.STRING,
-      remind: DataTypes.INTEGER,
-      paid: DataTypes.BOOLEAN
-    });
-    return Bills;
-  };
-  
+  var Bills = sequelize.define("Bills", {
+    userID: DataTypes.INTEGER,
+    name: DataTypes.STRING,
+    amount: DataTypes.INTEGER,
+    dueDay: DataTypes.INTEGER,
+    URL: DataTypes.STRING,
+    remind: DataTypes.INTEGER,
+    paid: DataTypes.BOOLEAN
+  });
+  return Bills;
+};
---
+++ models/seeds.sql
@@ -0,0 +1,3 @@
+/* dummy data for testing */
+USE billsdb;
+INSERT INTO Bills (userID, name, amount, dueDay, URL, remind, paid) VALUES (1, "cable", 200, 15, "https://www.spectrum.com/", 7, false);
